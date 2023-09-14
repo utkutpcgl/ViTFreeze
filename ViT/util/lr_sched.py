c2acb7e3d942c8a8f85d764c8a9c921e143d897b
monurcan <onurcan.kaya@kuartis.com>
2023-09-14 11:38:11 +0300

    ADD a note

--- ViT/util/lr_sched.py
+++ ViT/util/lr_sched.py
@@ -8,15 +8,13 @@ import math
 
 
 def adjust_learning_rate(optimizer, epoch, args):
-    # TODO left here
-    # TODO this adjust learning rate has to be adjusted to modify each layers learning rate based on max_j,j,layer_index,lr_ratio and lr.
-    """Decay the learning rate with half-cycle cosine after warmup"""
+    """Decay the learning rate with half-cycle cosine after warmup, step=epoch"""
     if epoch < args.warmup_epochs:
         lr = args.lr*epoch/args.warmup_epochs
     else:
         lr = args.min_lr+(args.lr-args.min_lr)*0.5*(1.+math.cos(math.pi*(epoch-args.warmup_epochs)/(args.epochs-args.warmup_epochs)))
     for param_group in optimizer.param_groups:
-        if "lr_scale" in param_group:
+        if "lr_scale" in param_group: # NOTE UTKU Only used for fine tuning.
             param_group["lr"] = lr * param_group["lr_scale"]
         else:
             param_group["lr"] = lr
