82bdf93f3dedff3fd407114c21dc0fe040592fcc
monurcan <onurcan.kaya@kuartis.com>
2023-09-14 11:36:44 +0300

    ADD adjusted update lr from freezout to local mim and comment out unused block

--- ViT/util/freezeout_utils.py
+++ ViT/util/freezeout_utils.py
@@ -4,44 +4,22 @@ from torch import nn
 
 
 # https://chat.openai.com/share/30777d71-4944-41ad-80a3-17dfca5bac7a
-# TODO left here
-class CustomBlock(Block):
-    def __init__(self, *args, layer_index=0, **kwargs):
-        super().__init__(*args, **kwargs)
+# TODO  is there a need for this block to detach and have freezeout attributes?? No I guess.
+# class CustomBlock(Block):
+#     def __init__(self, *args, layer_index=0, **kwargs):
+#         super().__init__(*args, **kwargs)
         
-        # Add your custom variables
-        self.active = True
-        self.layer_index = layer_index
+#         # Add your custom variables
+#         self.active = True
+#         self.layer_index = layer_index
 
-    def forward(self, x):
-        if self.active:
-            return super().forward(x)
-        else:
-            # Custom behavior when the block is inactive
-            # For example, you might want to detach the tensor to stop gradients
-            return x.detach()
-
-
-
-def update_lr(mae_vit_model, optim, initial_lr):
-        # Loop over all modules, requires -> self.j and module. active, max_j, layer_index, lr_ratio,
-        for m in mae_vit_model.modules():
-            # If a module is active:
-            if hasattr(m,'active') and m.active:
-                # If we've passed this layer's freezing point, deactivate it.
-                if mae_vit_model.j > m.max_j: 
-                    m.active = False
-                    # Also make sure we remove all this layer from the optimizer
-                    for i,group in enumerate(optim.param_groups):
-                        if group['layer_index']==m.layer_index:
-                            optim.param_groups.remove(group)
-                # If not, update the LR
-                else:
-                    for i,group in enumerate(optim.param_groups):
-                        if group['layer_index']==m.layer_index:
-                            optim.param_groups[i]['lr'] = ((initial_lr/2)/m.lr_ratio)*(1+np.cos(np.pi*mae_vit_model.j/m.max_j))\
-                                                              if mae_vit_model.scale_lr else (initial_lr/2) * (1+np.cos(np.pi*mae_vit_model.j/m.max_j))
-        mae_vit_model.j += 1  # TODO left here.
+#     def forward(self, x):
+#         if self.active:
+#             return super().forward(x)
+#         else:
+#             # Custom behavior when the block is inactive
+#             # For example, you might want to detach the tensor to stop gradients
+#             return x.detach()
 
 
 # NOTE checked oK.
@@ -122,3 +100,45 @@ def param_groups_weight_decay(
         {'params': no_decay, 'weight_decay': 0.},
         {'params': decay, 'weight_decay': weight_decay}]
 
+
+def adjust_learning_rate_fo(model, optimizer, epoch, cur_local_iteration, iter_per_epoch, args):
+    # TODO Double check here
+    """Freezeout decay the learning rate with half-cycle cosine after linnear warmup, step=iteration"""
+    total_warmup_iterations = iter_per_epoch*args.warmup_epochs
+    cur_global_iteration = cur_local_iteration + epoch*iter_per_epoch
+    if cur_global_iteration < total_warmup_iterations:
+        lr = args.lr*cur_global_iteration/total_warmup_iterations
+        for param_group in optimizer.param_groups:
+            if "lr_scale" in param_group: # NOTE UTKU Only used for fine tuning.
+                param_group["lr"] = lr * param_group["lr_scale"]
+            else:
+                param_group["lr"] = lr
+    else:
+        # NOTE not used anywhere, but initial lr after warmup is (Optionally scale the learning rates to have the same total):
+        # module.lr = args.lr / module.lr_ratio if scale_lr else args.lr # freezout specific,
+        update_lr(model, cur_global_iteration, optimizer, initial_lr=args.lr)
+
+def update_lr(model, cur_global_iteration, optim, initial_lr):
+        """
+        initial_lr: The default learning rate of the overall model before scaling (after warmup)
+        Here we assume the min_lr=0 in cosine annealing (orginally -> min_lr + (lr-min_lr)*...)
+        """
+        # NOTE cur_global_iteration incremented by train loop
+        # Loop over all modules, requires -> cur_global_iteration and module. active, max_iteration, layer_index, lr_ratio,
+        for m in model.modules():
+            # If a module is active:
+            if hasattr(m,'active') and m.active:
+                # If we've passed this layer's freezing point, deactivate it.
+                if cur_global_iteration > m.max_iteration: 
+                    m.active = False
+                    m.requires_grad = False # NOTE detach is no longer necessary in the forward passes.
+                    # Also make sure we remove all this layer from the optimizer
+                    for i,group in enumerate(optim.param_groups):
+                        if group['layer_index']==m.layer_index:
+                            optim.param_groups.remove(group)
+                # If not, update the LR
+                else:
+                    for i,group in enumerate(optim.param_groups):
+                        if group['layer_index']==m.layer_index:
+                            lr = initial_lr/m.lr_ratio if model.scale_lr else initial_lr
+                            optim.param_groups[i]['lr'] = (lr/2)*(1+np.cos(np.pi*cur_global_iteration/m.max_iteration))
