9459e16937071219b594c859d4a87ba66fe2a7a4
monurcan <onurcan.kaya@kuartis.com>
2023-10-24 13:26:53 +0300

    ADD iterate only over active layer modules, not whole modules

--- ViT/engine_pretrain.py
+++ ViT/engine_pretrain.py
@@ -23,7 +23,7 @@ import util.lr_sched as lr_sched
 import util.freezeout_utils as fo_sched
 
 
-def train_one_epoch(model, data_loader, optimizer, device, epoch, loss_scaler, param_groups, log_writer=None, args=None):
+def train_one_epoch(model, data_loader, optimizer, device, epoch, loss_scaler, param_groups, active_freezeout_modules, log_writer=None, args=None):
     model.train(True)
     metric_logger = misc.MetricLogger(delimiter="  ")
     metric_logger.add_meter('lr', misc.SmoothedValue(window_size=1, fmt='{value:.6f}'))
@@ -45,7 +45,7 @@ def train_one_epoch(model, data_loader, optimizer, device, epoch, loss_scaler, p
         if data_iter_step % accum_iter == 0:
             # Default was: lr_sched.adjust_learning_rate(optimizer, data_iter_step/len(data_loader)+epoch, args)
             # NOTE lr and attributes have to be set for all models (all ranks.)
-            fo_sched.adjust_learning_rate_freezeout(model, optimizer,  epoch, data_iter_step, param_groups, iter_per_epoch=len(data_loader), writer=log_writer, args=args) # Freezeout specific
+            fo_sched.adjust_learning_rate_freezeout(optimizer,  epoch, data_iter_step, param_groups, active_freezeout_modules=active_freezeout_modules, iter_per_epoch=len(data_loader), writer=log_writer, args=args) # Freezeout specific
 
         samples = samples.to(device, non_blocking=True)
         with torch.cuda.amp.autocast():
