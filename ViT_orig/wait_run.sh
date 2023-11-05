6f50eead8cab7c717b1a8b7812fa66daece6761f
monurcan <onurcan.kaya@kuartis.com>
2023-11-05 18:54:27 +0300

    ADD multi process option

--- ViT_orig/wait_run.sh
+++ ViT_orig/wait_run.sh
@@ -4,20 +4,40 @@
 REMOTE_HOST="192.168.1.100"
 REMOTE_USER="kuartis-dgx1"
 PASSWORD="kuartis2012"
-TARGET_PID=2659044
+TARGET_PID=1421782
+
+TARGET_PIDS=(1421782 2335973) # Add your PIDs here
 
 # Create an SSH command with sshpass
 SSH_COMMAND="sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -T ${REMOTE_USER}@${REMOTE_HOST}"
 
+# # Start the SSH session and the while loop on the remote host
+# $SSH_COMMAND << 'EOF'
+# # Loop through all the PIDs
+# for TARGET_PID in "${TARGET_PIDS[@]}"; do
+#   while ps -p $TARGET_PID > /dev/null 2>&1; do
+#       echo "Process $TARGET_PID is still running. Checking again in 1 seconds..."
+#       sleep 1
+#   done
+#   echo "Process $TARGET_PID has finished."
+# done
+# EOF
+
 # Start the SSH session and the while loop on the remote host
 $SSH_COMMAND << EOF
-while ps -p $TARGET_PID > /dev/null 2>&1; do
-    echo "Process $TARGET_PID is still running. Checking again in 1 seconds..."
-    sleep 1
+# Read the PIDs into an array on the remote shell
+read -a pid_array <<< "$TARGET_PIDS"
+# Loop through all the PIDs
+for TARGET_PID in "\${pid_array[@]}"; do
+  while ps -p \$TARGET_PID > /dev/null 2>&1; do
+      echo "Process \$TARGET_PID is still running. Checking again in 1 seconds..."
+      sleep 1
+  done
+  echo "Process \$TARGET_PID has finished."
 done
 EOF
 
-echo "Process $TARGET_PID has finished start training."
+echo "All processes have finished, start training."
 
 
 bash record.sh CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 OMP_NUM_THREADS=1 \
